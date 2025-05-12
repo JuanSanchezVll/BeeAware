e18165a9cff4b82354c99d0a4554170deb721f46
ainzenBleach <samuel.barros@sptech.school>
2025-05-11 22:16:33 -0300

    Atualização no codigo de arduino para sair 3 dados

--- Código Arduíno/arduino/arduino.ino
+++ Código Arduíno/arduino/arduino.ino
@@ -1,14 +1,25 @@
 // Declaração e atribuição de variaveis
 const int PINO_SENSOR_TEMPERATURA = A0;
-float temperaturaCelsius;
+float temperaturaCelsius1;
+float temperaturaCelsius2;
+float temperaturaCelsius3;
+int numeroAleatorio;
 void setup() {
   Serial.begin(9600);
+  randomSeed(analogRead(0));
 }
 
 void loop() {
+  numeroAleatorio = random(1, 11);
   int valorLeitura = analogRead(PINO_SENSOR_TEMPERATURA); //A0
-  temperaturaCelsius = ((valorLeitura * 5.0 / 1023.0) / 0.01) + 15;
-  Serial.println(temperaturaCelsius);
-
+  temperaturaCelsius1 = ((valorLeitura * 5.0 / 1023.0) / 0.01) + numeroAleatorio;
+  temperaturaCelsius2 = ((valorLeitura * 5.0 / 1023.0) / 0.01) + numeroAleatorio;
+  temperaturaCelsius3 = ((valorLeitura * 5.0 / 1023.0) / 0.01) + numeroAleatorio;
+  Serial.print(temperaturaCelsius1);
+  Serial.print(";");
+  Serial.print(temperaturaCelsius2);
+  Serial.print(",");
+  Serial.println(temperaturaCelsius3);
+  
   delay(2000);
 }
