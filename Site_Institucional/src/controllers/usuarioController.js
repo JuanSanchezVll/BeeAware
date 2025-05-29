c21021e6f47ff051652a351480e4679caa495ebf
Gabriel-Akira-Endo <gabriel.endo@sptech.school>
2025-05-28 23:35:36 -0300

    api login e cadastro

--- Site_Institucional/src/controllers/usuarioController.js
+++ Site_Institucional/src/controllers/usuarioController.js
@@ -16,9 +16,13 @@ function autenticar(req, res) {
                     console.log(`\nResultados encontrados: ${resultadoAutenticar.length}`);
                     console.log(`Resultados: ${JSON.stringify(resultadoAutenticar)}`); // transforma JSON em String
 
-                     if (resultadoAutenticar.length == 0) {
+                    if (resultadoAutenticar.length == 0) {
                         res.status(403).send("Email e/ou senha inválido(s)");
-                    } else {
+                    }
+                    else if (resultadoAutenticar.length == 1) {
+                        res.status(200).json(resultadoAutenticar[0]);
+                    }
+                    else {
                         res.status(403).send("Mais de um usuário com o mesmo login e senha!");
                     }
                 }
@@ -35,28 +39,34 @@ function autenticar(req, res) {
 
 function cadastrar(req, res) {
     // Crie uma variável que vá recuperar os valores do arquivo cadastro.html
-    var nome = req.body.nomeServer;
-    var email = req.body.emailServer;
-    var cnpj  = req.body.cnpjServer;
+    var nome = req.body.nomeServer
+    var email = req.body.emailServer
+    var tel = req.body.telServer
+    var cep = req.body.cepServer
+    var num = req.body.numServer
+    var comp = req.body.compServer
+    var cnpj = req.body.cnpjServer
+    var senha = req.body.senhaServer
 
-        // Passe os valores como parâmetro e vá para o arquivo usuarioModel.js
-        usuarioModel.cadastrar(nome, email, cnpj)
 
-            .then(
-                function (resultado) {
-                    res.json(resultado);
-                }
-            ).catch(
-                function (erro) {
-                    console.log(erro);
-                    console.log(
-                        "\nHouve um erro ao realizar o cadastro! Erro: ",
-                        erro.sqlMessage
-                    );
-                    res.status(500).json(erro.sqlMessage);
-                }
-            );
-    }
+    // Passe os valores como parâmetro e vá para o arquivo usuarioModel.js
+    usuarioModel.cadastrar(nome, email, cnpj, tel, cep, num, comp, senha)
+
+        .then(
+            function (resultado) {
+                res.json(resultado);
+            }
+        ).catch(
+            function (erro) {
+                console.log(erro);
+                console.log(
+                    "\nHouve um erro ao realizar o cadastro! Erro: ",
+                    erro.sqlMessage
+                );
+                res.status(500).json(erro.sqlMessage);
+            }
+        );
+}
 
 
 module.exports = {
--- Site_Institucional/src/database/script-tabelas.sql
+++ Site_Institucional/src/database/script-tabelas.sql
@@ -1,64 +1,138 @@
--- Arquivo de apoio, caso você queira criar tabelas como as aqui criadas para a API funcionar.
--- Você precisa executar os comandos no banco de dados para criar as tabelas,
--- ter este arquivo aqui não significa que a tabela em seu BD estará como abaixo!
+CREATE DATABASE BeeAware;
 
-/*
-comandos para mysql server
-*/
+USE BeeAware;
 
-CREATE DATABASE aquatech;
-select * from usuario;
-USE aquatech;
-
+/*-------------------------------------------------------------------------------------------- Criação da tabela empresa --------------------------------------------------------------------------------------------------------------- */
+-- Criação da tabela empresa, onde serão amazenados os dados de cada empresa.
 CREATE TABLE empresa (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	razao_social VARCHAR(50),
-	cnpj CHAR(14),
-	codigo_ativacao VARCHAR(50)
+    idEmpresa INT PRIMARY KEY AUTO_INCREMENT,
+    nome varchar(45) not null,
+    cnpj VARCHAR(18) not null,
+    telefone VARCHAR(14),
+    email VARCHAR(80) not null,
+    cep CHAR(9) not null,
+    numero INT not null,
+    complemento VARCHAR(255) not null,
+    senha varchar(45)
 );
+
+DESCRIBE empresa;
+
+SELECT * FROM empresa;
+
+/*-------------------------------------------------------------------------------------------- Criação da tabela usuário --------------------------------------------------------------------------------------------------------------- */
+-- Criação da tabela usuário, onde serão armazenados os dados de acesso.
 
 CREATE TABLE usuario (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	nome VARCHAR(50),
-	email VARCHAR(50),
-	senha VARCHAR(50),
-	fk_empresa INT,
-	FOREIGN KEY (fk_empresa) REFERENCES empresa(id)
+    idUsuario INT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(255),
+    senha VARCHAR(255),
+    cpf VARCHAR(15),
+    email VARCHAR(80),
+    cargo VARCHAR(8),
+    fkEmpresa INT,
+    CONSTRAINT fkUserEmpresa FOREIGN KEY (fkEmpresa)
+        REFERENCES empresa (idEmpresa)
 );
 
-CREATE TABLE aviso (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	titulo VARCHAR(100),
-	descricao VARCHAR(150),
-	fk_usuario INT,
-	FOREIGN KEY (fk_usuario) REFERENCES usuario(id)
+DESCRIBE usuario;
+
+SELECT * FROM usuario;
+
+/*-------------------------------------------------------------------------------------------- Criação da tabela setor --------------------------------------------------------------------------------------------------------------- */
+-- Criação da tabela setor, onde serão armazenados dos dados de cada setor, relacionados com a sua respectiva empresa. 
+
+CREATE TABLE setor (
+    idSetor INT PRIMARY KEY AUTO_INCREMENT,
+    setor VARCHAR(45),
+    fkEmpresa INT,
+    CONSTRAINT fkSetorEmpresa FOREIGN KEY (fkEmpresa)
+        REFERENCES empresa (idEmpresa)
 );
 
-create table aquario (
-/* em nossa regra de negócio, um aquario tem apenas um sensor */
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	descricao VARCHAR(300),
-	fk_empresa INT,
-	FOREIGN KEY (fk_empresa) REFERENCES empresa(id)
+DESCRIBE setor;
+
+SELECT * FROM setor;
+
+/*-------------------------------------------------------------------------------------------- Criação da tabela apiario --------------------------------------------------------------------------------------------------------------- */
+-- Criação da tabela apiário, onde serão armazenados os dados de identificação de cada apiário, relacionados com o setor em que estão localizados.
+
+CREATE TABLE apiario (
+    idApiario INT PRIMARY KEY AUTO_INCREMENT,
+    identificador_colonia VARCHAR(45),
+    fkSetor INT,
+    CONSTRAINT fkSetorSensor FOREIGN KEY (fkSetor)
+        REFERENCES setor (idSetor)
 );
 
-/* esta tabela deve estar de acordo com o que está em INSERT de sua API do arduino - dat-acqu-ino */
+DESCRIBE apiario;
 
-create table medida (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	dht11_umidade DECIMAL,
-	dht11_temperatura DECIMAL,
-	luminosidade DECIMAL,
-	lm35_temperatura DECIMAL,
-	chave TINYINT,
-	momento DATETIME,
-	fk_aquario INT,
-	FOREIGN KEY (fk_aquario) REFERENCES aquario(id)
+SELECT * FROM apiario;
+
+/*-------------------------------------------------------------------------------------------- Criação da tabela sensor --------------------------------------------------------------------------------------------------------------- */
+-- Criação da tabela sensor, onde serão armazenados os dados coletados por cada sensor, seu status de funcionamento e o apiário em que está instalado.
+
+CREATE TABLE sensor (
+    idSensor INT PRIMARY KEY AUTO_INCREMENT,
+    numSerie VARCHAR(20),
+    codInterno VARCHAR(20),
+    statusSen VARCHAR(20),
+    CONSTRAINT chkStatus CHECK (statusSen IN ('Ativo' , 'Inativo', 'Manutenção', 'Com problema')),
+    fkApiario int,
+    CONSTRAINT fkSensorApiario FOREIGN KEY (fkApiario)
+		REFERENCES apiario(idApiario)
 );
-insert into medida (dht11_umidade, dht11_temperatura, momento,fk_aquario ) value
-('80',"27",current_time(),'2');
-select * from medida;
-insert into empresa (razao_social, codigo_ativacao) values ('Empresa 1', 'ED145B');
-insert into empresa (razao_social, codigo_ativacao) values ('Empresa 2', 'A1B2C3');
-insert into aquario (descricao, fk_empresa) values ('Aquário de Estrela-do-mar', 1);
-insert into aquario (descricao, fk_empresa) values ('Aquário de Peixe-dourado', 2);
+
+DESCRIBE sensor;
+
+SELECT * FROM sensor;
+
+/*-------------------------------------------------------------------------------------------- Criação da tabela leitura --------------------------------------------------------------------------------------------------------------- */
+-- Criação da tabela leitura, onde serão armazenados os dados das leituras feitas pelo sensor, e qual sensor fez a leitura.
+
+CREATE TABLE leitura (
+    idLeitura INT PRIMARY KEY AUTO_INCREMENT,
+    temperatura DECIMAL(3 , 1),
+    dtLeitura DATETIME,
+    fkSensor INT,
+    CONSTRAINT fkLeituraSensor FOREIGN KEY (fkSensor)
+        REFERENCES sensor (idSensor)
+);
+
+DESCRIBE leitura;
+
+SELECT * FROM leitura;
+
+/*-------------------------------------------------------------------------------------------- Criação da tabela Recomendação --------------------------------------------------------------------------------------------------------------- */
+-- Criação da tabela recomendação, onde serão armazenados os dados das recomendações geradas pelo sistema.
+
+CREATE TABLE recomendacao(
+idRecomendacao int primary key auto_increment,
+recomendacao varchar(45)
+);
+
+DESCRIBE recomendacao;
+
+SELECT * FROM recomendacao;
+
+/*-------------------------------------------------------------------------------------------- Criação da tabela recomendação da Leitura --------------------------------------------------------------------------------------------------------------- */
+-- Criação da tabela auxiliar da tabela leitura, onde serão armazenados os dados para identificação da recomendação para cada sensor.
+
+CREATE TABLE recomendacaoLeitura(
+fkLeitura int,
+fkSensor int,
+fkRecomendacao int,
+dtRecomendacao datetime,
+constraint chaveComposta primary key(fkLeitura, fkSensor,fkRecomendacao),
+foreign key (fkLeitura) references leitura(idLeitura),
+foreign key (fkSensor) references sensor(idSensor),
+foreign key (fkRecomendacao) references recomendacao(idRecomendacao)
+);
+
+DESCRIBE recomendacaoLeitura;
+
+SELECT * FROM recomendacaoLeitura;
+
+
+
+
