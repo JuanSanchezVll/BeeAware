0b8e099af3c6cbad7cd4be191e97d49c41ec8177
figuegab <gabriel.figueiredo@sptech.school>
2025-06-12 14:12:00 -0300

    Adicionando método de encriptação MD5 para senha

--- Site_Institucional/src/models/usuarioModel.js
+++ Site_Institucional/src/models/usuarioModel.js
@@ -4,7 +4,7 @@ var database = require("../database/config")
 function autenticar(email, senha) {
     console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function entrar(): ", email, senha)
     var instrucaoSql = `
-        SELECT idEmpresa, nome, email FROM empresa WHERE email = '${email}' AND senha = '${senha}';
+        SELECT idEmpresa, nome, email FROM empresa WHERE email = '${email}' AND senha = MD5('${senha}');
     `; 
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
     return database.executar(instrucaoSql);
@@ -13,7 +13,7 @@ function autenticar(email, senha) {
 // Coloque os mesmos parâmetros aqui. Vá para a var instrucaoSql
 function cadastrar(nome, email, cnpj, tel, cep, num, comp, senha) {
     var instrucaoSql = `
-        INSERT INTO empresa VALUES (default,'${nome}', '${cnpj}', '${tel}','${email}', '${cep};', '${num}', '${comp}','${senha}');
+        INSERT INTO empresa VALUES (default,'${nome}', '${cnpj}', '${tel}','${email}', '${cep};', '${num}', '${comp}', MD5('${senha}'));
     `;
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
     return database.executar(instrucaoSql);
